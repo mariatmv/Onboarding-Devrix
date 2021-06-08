025c2f9ac3cff8b7cdffcea9719d8f019a83d1eb
Maria Tomovich <tomovich47@gmail.com>
2021-06-08 19:59:17 +0300

    quick fixes and adding comments

--- wp-content/plugins/amazon_search/amazon_search.php
+++ wp-content/plugins/amazon_search/amazon_search.php
@@ -17,7 +17,6 @@ function amazon_search_options() {
 		wp_die('You do not have permission to access this page');
 	}
 	?>
-    <br><br><br>
     <input type="url" id="amazonUrl" name="amazonUrl">
     <label for="duration">Choose duration (in seconds):</label>
     <select id="transientDuration" name="duration">
@@ -26,8 +25,7 @@ function amazon_search_options() {
         <option value="20">20</option>
     </select>
     <button id="searchUrlBtn">Search</button>
-    <br>
-    <div id="container"><?php echo cache_results_display(); ?></div>
+    <div id="container"><?php  cache_results_display(); ?></div>
    <?php
 }
 
@@ -44,11 +42,12 @@ add_action( 'wp_ajax_amazon_search', 'amazon_search' );
 function amazon_search() {
 	$url = $_POST['data']['amazon_url'];
 	$duration_in_seconds = $_POST['data']['transient_duration'];
-	setting_url_transient($url, $duration_in_seconds);
+	setting_url_and_duration_transient($url, $duration_in_seconds);
 	display_results();
 	wp_die();
 }
 
+/** Checks if we have transient and calls the display function if we do */
 function cache_results_display() {
 	$result = get_transient( 'search_results' );
 	if ( false !== $result ){
@@ -56,12 +55,21 @@ function cache_results_display() {
 	}
 }
 
-function setting_url_transient($url, $duration_in_seconds) {
-    set_transient('search_results', $url, $duration_in_seconds);
+/** Sets the url and the duration in transient */
+function setting_url_and_duration_transient($url, $duration_in_seconds) {
+    if ($url !== '') {
+        if (!$duration_in_seconds) {
+            $duration_in_seconds = 10;
+        }
+	    set_transient('search_results', $url, $duration_in_seconds);
+    }
 }
 
+/** Displays the results to the user */
 function display_results() {
-    echo wp_remote_request(get_transient('search_results'))['body'];
+    $url = get_transient('search_results');
+    $contents = file_get_contents($url);
+    echo $contents;
 }
 
 ?>
